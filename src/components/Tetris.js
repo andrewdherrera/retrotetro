bd24f42cee23790eeacdda7251b67c6074938341
andrewdherrera <thiinblueline2830@gmail.com>
2020-04-03 18:15:58 -0500

    Created react/hooks files. Starting to build collision code ito bring project together.

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,5 +1,6 @@
-import React from 'react';
- 
+import React, { useState } from 'react';
+import { usePlayer } from '../hooks/usePlayer';
+import { useStage } from '../hooks/useStage';
 import { createStage } from '../gameHelpers';
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris' 
 import Stage from './Stage';
@@ -7,16 +8,30 @@ import Display from './Display';
 import StartButton from './StartButton';
 
 const Tetris = () => {
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  const [player] = usePlayer();
+  const [stage, setStage] = useStage(player);
+
+
+
+
+    console.log('re-render');
     return (
         <StyledTetrisWrapper>
           <StyledTetris>
-            <Stage stage={createStage()}/>
+            <Stage stage={stage}/>
             <aside>
+                {gameOver ? (
+                    <Display gameOver={gameOver} text="Game Over" />
+                ) : (
                 <div>
-                <Display text="Score" />
-                <Display text="Rows" />
-                <Display text="Levels" />
+                  <Display text="Score" />
+                  <Display text="Rows" />
+                  <Display text="Levels" />
                 </div>
+                )}
                 <StartButton />
             </aside>
           </StyledTetris>
