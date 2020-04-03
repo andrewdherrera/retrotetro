208410e9a023600dd56d81c1ca02844cc87a9d54
andrewdherrera <thiinblueline2830@gmail.com>
2020-04-03 14:57:31 -0500

    Completed styling of stage and components for rendering.

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,19 +1,16 @@
 import React from 'react';
  
 import { createStage } from '../gameHelpers';
-
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris' 
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
 
 const Tetris = () => {
-
-
-
     return (
-        <div>
+        <StyledTetrisWrapper>
+          <StyledTetris>
             <Stage stage={createStage()}/>
-
             <aside>
                 <div>
                 <Display text="Score" />
@@ -22,10 +19,9 @@ const Tetris = () => {
                 </div>
                 <StartButton />
             </aside>
-        </div>
-
+          </StyledTetris>
+        </StyledTetrisWrapper>
     )
-
 }
 
 export default Tetris;
